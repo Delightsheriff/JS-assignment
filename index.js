d4ee4ee3777461477d1aec5fc8acdca7ec4db146
Delight Amadi-Sheriff <87504744+Delightsheriff@users.noreply.github.com>
2023-10-12 19:47:04 +0100

    map, filter and forEach

--- index.js
+++ index.js
@@ -98,6 +98,7 @@ if (jambStudents[3].score >= 70) {
   );
 }
 */
+/*
 const student = {
   name: "Amadi Sheriff Delight",
   age: 27,
@@ -120,3 +121,138 @@ const gradeChecker = (student) => {
   console.log(`${student.name} is not eligible for any grade.`);
 };
 gradeChecker(student);
+*/
+//map, filter and forEach
+const students = [
+  {
+    name: "arun",
+    gender: "Male",
+    physics: 88,
+    maths: 87,
+    english: 78,
+  },
+  {
+    name: "rajesh",
+    gender: "Male",
+    physics: 96,
+    maths: 100,
+    english: 95,
+  },
+  {
+    name: "moorthy",
+    gender: "Male",
+    physics: 89,
+    maths: 90,
+    english: 70,
+  },
+  {
+    name: "raja",
+    gender: "Male",
+    physics: 30,
+    maths: 25,
+    english: 40,
+  },
+  {
+    name: "usha",
+    gender: "Female",
+    physics: 67,
+    maths: 45,
+    english: 78,
+  },
+  {
+    name: "priya",
+    gender: "Female",
+    physics: 56,
+    maths: 46,
+    english: 78,
+  },
+  {
+    name: "Sundar",
+    gender: "Male",
+    physics: 12,
+    maths: 67,
+    english: 67,
+  },
+  {
+    name: "Kavitha",
+    gender: "Female",
+    physics: 78,
+    maths: 71,
+    english: 67,
+  },
+  {
+    name: "Dinesh",
+    gender: "Male",
+    physics: 56,
+    maths: 45,
+    english: 67,
+  },
+  {
+    name: "Hema",
+    gender: "Female",
+    physics: 71,
+    maths: 90,
+    english: 23,
+  },
+  {
+    name: "Gowri",
+    gender: "Female",
+    physics: 100,
+    maths: 100,
+    english: 100,
+  },
+  {
+    name: "Ram",
+    gender: "Male",
+    physics: 78,
+    maths: 55,
+    english: 47,
+  },
+  {
+    name: "Murugan",
+    gender: "Male",
+    physics: 34,
+    maths: 89,
+    english: 78,
+  },
+  {
+    name: "Jenifer",
+    gender: "Female",
+    physics: 67,
+    maths: 88,
+    english: 90,
+  },
+];
+//1). using forEach, calculate everybods average,
+students.forEach((student) => {
+  student.average = (student.english + student.physics + student.maths) / 3;
+  console.log(student.average);
+});
+// students;
+//2). after calculating the average, filter only average with above 100
+let above_100 = students.filter((student) => {
+  return student.average >= 100;
+});
+//3). after calcukating average, and filtering, return the names
+let above_100_student = above_100.map((student) => {
+  console.log(student.name);
+});
+
+//4). return male
+let Males = students
+  .filter((student) => {
+    return student.gender === "Male";
+  })
+  .map((student) => {
+    console.log(student.name);
+  });
+
+//5). return female
+let Females = students
+  .filter((student) => {
+    return student.gender === "Female";
+  })
+  .map((student) => {
+    console.log(student.name);
+  });
+
